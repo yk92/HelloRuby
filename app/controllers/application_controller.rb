c327d6f93cecfe54d9120adf5f88e92ec70466dd
Yuval Klein <yk92@njit.edu>
2016-05-27 15:29:21 -0400

    Changed Hello to Goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def hello
-    render text: "Hello, World!"
+  def goodbye
+    render text: "Goodbye, Cruel World!"
   end
 end
